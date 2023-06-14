0086e50ee7fbbaf7f3f887cce742be6824d313a1
tugonbob <joshuakgao@gmail.com>
2023-06-14 18:04:59 -0500

    Fixed spacing on smaller screens

--- src/pages/sections/ContactSection.tsx
+++ src/pages/sections/ContactSection.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FlexDiv, RowDiv, Spacer } from "../../components/commonComponents";
 
 export function ContactSection() {
@@ -6,55 +6,59 @@ export function ContactSection() {
     <FlexDiv
       style={{ backgroundColor: "white", padding: 64, alignItems: "center" }}
     >
-      <FlexDiv style={{ minWidth: 800, alignItems: "center" }}>
-        <FlexDiv style={{ alignItems: "center" }}>
-          <h2 className="tertiary">CONTACT US</h2>
-          <Spacer size={64} />
-          <h2 style={{ margin: 0 }}>
-            Let's build something beautiful{" "}
-            <b
-              style={{
-                color: "var(--accent)",
-                fontFamily: "Caveat",
-                fontSize: 48,
-              }}
-            >
-              together
-            </b>
-          </h2>
-        </FlexDiv>
-        <Spacer size={32} />
+      <FlexDiv style={{ alignItems: "center", width: "100%" }}>
+        <h2 className="tertiary">CONTACT US</h2>
+        <Spacer size={64} />
+        <h2 style={{ margin: 0 }}>
+          Let's build something beautiful{" "}
+          <b
+            style={{
+              color: "var(--accent)",
+              fontFamily: "Caveat",
+              fontSize: 48,
+            }}
+          >
+            together
+          </b>
+        </h2>
+      </FlexDiv>
+      <Spacer size={32} />
+      <FlexDiv
+        style={{
+          flexDirection: "row",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+          padding: 64,
+          borderRadius: "var(--borderRadius)",
+          flexWrap: "wrap",
+        }}
+      >
         <FlexDiv
           style={{
-            flexDirection: "row",
-            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-            padding: 64,
-            borderRadius: "var(--borderRadius)",
+            maxWidth: 450,
+            marginBottom: 32,
+            marginRight: "5vw",
           }}
         >
-          <FlexDiv style={{ maxWidth: 450 }}>
-            <h2 style={{ margin: 0 }}>Email Us</h2>
-            <p>
-              Please provide your <b>name</b>, <b>email</b>, and <b>budget</b>.
-              Then, provide a <b>project description</b> and your desired{" "}
-              <b>project completion date</b>.
-            </p>
-            <h3>What's Next?</h3>
-            <p className="tertiary">
-              Next we’ll set up a video call where you can tell us about your
-              project, get to know us, ask questions, get a price quote, and get
-              a completion date.
-            </p>
-          </FlexDiv>
-          <Spacer horizontal size={128} />
-          <FlexDiv style={{ maxWidth: 300 }}>
-            <h4>Contact Info</h4>
-            <p style={{ color: "purple" }}>sigmoid.studios.llc@gmail.com</p>
-            <Spacer />
-            <h4>Office Hours</h4>
-            <p>Monday - Friday</p>
-            <p style={{ marginTop: 8 }}>9am - 4:30am (CST)</p>
-          </FlexDiv>
+          <h2 style={{ margin: 0 }}>Email Us</h2>
+          <p>
+            Please provide your <b>name</b>, <b>email</b>, and <b>budget</b>.
+            Then, provide a <b>project description</b> and your desired{" "}
+            <b>project completion date</b>.
+          </p>
+          <h3>What's Next?</h3>
+          <p className="tertiary">
+            Next we’ll set up a video call where you can tell us about your
+            project, get to know us, ask questions, get a price quote, and get a
+            completion date.
+          </p>
+        </FlexDiv>
+        <FlexDiv style={{ maxWidth: 300 }}>
+          <h4>Contact Info</h4>
+          <p style={{ color: "purple" }}>sigmoid.studios.llc@gmail.com</p>
+          <Spacer />
+          <h4>Office Hours</h4>
+          <p>Monday - Friday</p>
+          <p style={{ marginTop: 8 }}>9am - 4:30am (CST)</p>
         </FlexDiv>
       </FlexDiv>
       <Spacer size={128} />
