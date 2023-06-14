ebe46effbc5ab177fbd4fa454fdd083ff7afd4b4
tugonbob <joshuakgao@gmail.com>
2023-06-13 23:33:14 -0500

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./assets/styles.css";
-import { Navbar } from "./components/customComponents/navbar/navbar";
-import {
-  LinearRegressionPage,
-  StanfordCs229Page,
-} from "./components/projectComponents/aiml";
-import {
-  AskGptPage,
-  JoshgaocomPage,
-} from "./components/projectComponents/appdev";
-import {
-  PortfolioTrackerPage,
-  RealEstateDealFinderPage,
-  RedditLaughsPage,
-} from "./components/projectComponents/other";
 import { HomePage } from "./pages";
 // @ts-ignore
 import { MathJaxContext } from "better-react-mathjax";
