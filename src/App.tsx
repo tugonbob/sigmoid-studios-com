d183b3b0f3b05184ebc2e1b1999001bd9562d7c1
tugonbob <joshuakgao@gmail.com>
2023-06-18 22:35:07 -0500

    Removed unused npm packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./assets/styles.css";
 import { HomePage } from "./pages";
-// @ts-ignore
-import { MathJaxContext } from "better-react-mathjax";
 
 function App() {
   return (
-    <MathJaxContext>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </MathJaxContext>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
